65961e2910f3821631d6726574f683469320af92
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-11-01 12:29:11 +0600

    MainContainer Created By Using Styled Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
+import styled from "styled-components";
+import { FaFacebookF, FaInstagram, FaTwitter } from "react-icons/fa";
 
 const App = () => {
-	return <div className="text-7xl font-bold">App</div>;
+	return <MainContainer className="text-7xl font-bold"></MainContainer>;
 };
+
+const MainContainer = styled.div`
+	width: 30vw;
+	height: 80vh;
+	display: flex;
+	align-items: center;
+	flex-direction: column;
+   background: rgba(255, 255, 255, 0.15);
+`;
 
 export default App;
