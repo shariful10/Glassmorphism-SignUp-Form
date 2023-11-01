2ac67d64707f007b4d76a2eaee68230c3f7ac180
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-11-01 12:42:08 +0600

    Implemented Some Style Components

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,11 @@ import styled from "styled-components";
 import { FaFacebookF, FaInstagram, FaTwitter } from "react-icons/fa";
 
 const App = () => {
-	return <MainContainer className="text-7xl font-bold"></MainContainer>;
+	return (
+		<MainContainer className="text-7xl font-bold">
+			<WelcomeText>Welcome</WelcomeText>
+		</MainContainer>
+	);
 };
 
 const MainContainer = styled.div`
@@ -11,7 +15,61 @@ const MainContainer = styled.div`
 	display: flex;
 	align-items: center;
 	flex-direction: column;
-   background: rgba(255, 255, 255, 0.15);
+	background: rgba(255, 255, 255, 0.15);
+	box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
+	backdrop-filter: blur(8.5px);
+	-webkit-backdrop-filter: blur(8.5px);
+	border-radius: 10px;
+	color: #ffffff;
+	text-transform: uppercase;
+	letter-spacing: 0.4rem;
+`;
+
+const WelcomeText = styled.h2`
+	margin: 3rem 0 2rem 0;
+`;
+
+const InputContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: space-around;
+	align-items: center;
+	height: 20%;
+	width: 100%;
+`;
+
+const ButtonContainer = styled.div`
+	margin: 1rem 0 2rem 0;
+	width: 100%;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+`;
+
+const LoginWith = styled.h5`
+	cursor: pointer;
+`;
+
+const HorizontalRule = styled.hr`
+	width: 90%;
+	height: 0.3rem;
+	border-radius: 0.8rem;
+	border: none;
+	background: linear-gradient(to right, #14163c 0%, #03217b 79%);
+	background-color: #ebd0d0;
+	margin: 1.5rem 0 1rem 0;
+	backdrop-filter: blur(25px);
+`;
+
+const IconsContainer = styled.div`
+	display: flex;
+	justify-content: space-evenly;
+	margin: 2rem 0 3rem 0;
+	width: 80%;
+`;
+
+const ForgotPassword = styled.h4`
+	cursor: pointer;
 `;
 
 export default App;
