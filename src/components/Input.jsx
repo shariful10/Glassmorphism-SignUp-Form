067b78611289567e4bfce1e91f2c1823cf8a343b
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-11-01 16:41:15 +0600

    Created Icon.jsx Component

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -4,17 +4,17 @@ export default function Input({ type, placeholder }) {
 }
 
 const StyledInput = styled.input`
-	background: rgba(255, 255, 255, 0.15);
-	box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
-	border-radius: 2rem;
 	width: 80%;
 	height: 3rem;
-	padding: 1rem;
 	border: none;
+	padding: 1rem;
 	outline: none;
 	color: #3c354e;
 	font-size: 1rem;
 	font-weight: bold;
+	border-radius: 2rem;
+	background: rgba(255, 255, 255, 0.15);
+	box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
 	&:focus {
 		display: inline-block;
 		box-shadow: 0 0 0 0.2rem #b9abe0;
